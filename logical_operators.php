5f696aaabf3c786240511dd77ab7670034d05517
tamuramasumi <tngu09010108@gmail.com>
2022-10-11 11:58:10 +0000

    logical_operators/php書いた

---
+++ logical_operators.php
@@ -0,0 +1,23 @@
+<?php
+
+//
+echo "論理演算子の確認\n";
+var_dump( true && false );
+var_dump( true || false );
+//ユーザーの情報
+$age = 20;
+$point = 499;
+
+//
+echo "\n";
+echo "「かつ」の場合\n";
+var_dump( ($age >= 20)&&($point >= 500) );
+
+echo "\n";
+echo "「または」の場合\n";
+var_dump( ($age >= 20)||($point >= 500) );
+
+
+
+
+
