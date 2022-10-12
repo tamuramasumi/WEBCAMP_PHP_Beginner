43faa6f20233a2d6d9919e5bdcdf3129336457bb
tamuramasumi <tngu09010108@gmail.com>
2022-10-12 07:28:22 +0000

    for.php書いた

--- sub_test.php
+++ sub_test.php
@@ -4,3 +4,20 @@ $family_name = "tamura";
 $first_name = "masumi";
 echo "{$family_name} {$first_name} \n";
 
+$i = 123;
+
+if ($i <= 100) {
+    echo "100以下 \n";
+} else {
+    echo "100を超える \n";
+}
+
+if ($i < 50) {
+    echo "50未満 \n";
+} else if ($i < 100) {
+    echo "100未満 \n";
+} else if ($i < 150) {
+    echo "150未満 \n";
+} else {
+    echo "150以上 \n";
+}
