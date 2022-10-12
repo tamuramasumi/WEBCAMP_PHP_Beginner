fe411d092c99abf02fdd71bce8f1b9c3dbf038f6
tamuramasumi <tngu09010108@gmail.com>
2022-10-12 06:10:59 +0000

    switch.php書いた

---
+++ switch.php
@@ -0,0 +1,32 @@
+<?php
+//
+$i = 1;
+switch($i) {
+    case 0:
+        echo "iは0です\n";
+        break;
+        
+    case 1:
+        echo "iは1です \n";
+        break;
+        
+    default:
+        echo "iは0と1以外です \n";
+        break;
+}
+
+//
+$i = 1;
+switch($i) {
+    case 0:
+        echo "i は 0です \n";
+        break;
+
+    case 1:
+        echo "i は 1です \n";
+        break;
+
+    default:
+        echo "i は 0と1 以外です \n";
+        break;
+}
