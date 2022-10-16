a821abe25454d26a69a6b3d1b22f06e63544a6ce
tamuramasumi <tngu09010108@gmail.com>
2022-10-16 00:07:46 +0000

    variables_scope.php書いた

---
+++ variables_scope.php
@@ -0,0 +1,19 @@
+<?php
+//変数への代入
+$a = 10;
+//変数の使用：使える
+echo "global:a is {$a} \n";
+
+//関数の宣言
+function myfunc()
+{
+    //変数への代入
+    $b = 999;
+    //変数の使用：使える
+    echo "in function:b is {$b} \n";
+    // 変数の使用：使えない
+    echo "in function:a is {$a} \n";
+}
+//関数の呼び出し
+myfunc();
+
